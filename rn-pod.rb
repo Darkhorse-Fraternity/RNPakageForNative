f2c56f578e1313ed486094c8de7b8e6b97069a1d
林通 <lintong320@gmail.com>
2019-09-26 15:17:41 +0800

    升级版本

--- rn-pod.rb
+++ rn-pod.rb
@@ -2,9 +2,14 @@ def use_rn_pod!(root = ".", config = nil)
 
     require  root + '/node_modules/@react-native-community/cli-platform-ios/native_modules'
 
+    pod 'FBLazyVector', :path =>  root + "/node_modules/react-native/Libraries/FBLazyVector"
+    pod 'FBReactNativeSpec', :path => root + "/node_modules/react-native/Libraries/FBReactNativeSpec"
+    pod 'RCTRequired', :path => root + "/node_modules/react-native/Libraries/RCTRequired"
+    pod 'RCTTypeSafety', :path => root +  "/node_modules/react-native/Libraries/TypeSafety"
     pod 'React', :path => root + '/node_modules/react-native/'
-    pod 'React-Core', :path => root + '/node_modules/react-native/React'
-    pod 'React-DevSupport', :path => root +'/node_modules/react-native/React'
+    pod 'React-Core', :path => root + '/node_modules/react-native/'
+    pod 'React-CoreModules', :path => root + '/node_modules/react-native/React/CoreModules'
+    pod 'React-Core/DevSupport', :path => root + '/node_modules/react-native/'
     pod 'React-RCTActionSheet', :path => root + '/node_modules/react-native/Libraries/ActionSheetIOS'
     pod 'React-RCTAnimation', :path => root + '/node_modules/react-native/Libraries/NativeAnimation'
     pod 'React-RCTBlob', :path => root + '/node_modules/react-native/Libraries/Blob'
@@ -14,15 +19,16 @@ def use_rn_pod!(root = ".", config = nil)
     pod 'React-RCTSettings', :path => root + '/node_modules/react-native/Libraries/Settings'
     pod 'React-RCTText', :path => root + '/node_modules/react-native/Libraries/Text'
     pod 'React-RCTVibration', :path => root + '/node_modules/react-native/Libraries/Vibration'
-    pod 'React-RCTWebSocket', :path => root + '/node_modules/react-native/Libraries/WebSocket'
-  
-  
+    pod 'React-Core/RCTWebSocket', :path => root + '/node_modules/react-native/'
+
     pod 'React-cxxreact', :path => root + '/node_modules/react-native/ReactCommon/cxxreact'
     pod 'React-jsi', :path => root + '/node_modules/react-native/ReactCommon/jsi'
     pod 'React-jsiexecutor', :path => root + '/node_modules/react-native/ReactCommon/jsiexecutor'
     pod 'React-jsinspector', :path => root + '/node_modules/react-native/ReactCommon/jsinspector'
-    pod 'yoga', :path => root + '/node_modules/react-native/ReactCommon/yoga'
-  
+    pod 'ReactCommon/jscallinvoker', :path => root + "/node_modules/react-native/ReactCommon"
+    pod 'ReactCommon/turbomodule/core', :path => root + "/node_modules/react-native/ReactCommon"
+    pod 'Yoga', :path => root + '/node_modules/react-native/ReactCommon/yoga'
+
     pod 'DoubleConversion', :podspec => root + '/node_modules/react-native/third-party-podspecs/DoubleConversion.podspec'
     pod 'glog', :podspec => root + '/node_modules/react-native/third-party-podspecs/glog.podspec'
     pod 'Folly', :podspec => root + '/node_modules/react-native/third-party-podspecs/Folly.podspec'
