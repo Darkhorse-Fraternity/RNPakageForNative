d2a4f25dd596954a25ec44da94f5595394bd4a05
林通 <lintong320@gmail.com>
2019-09-27 17:07:49 +0800

    添加目标target config

---
+++ targetRNConfig/react-native.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  dependencies: {
+    'react-native-gesture-handler': {},
+    'react-native-device-info': {
+      platforms: {
+        android: {
+          packageInstance: 'new RNDeviceInfo(false)',
+        },
+      },
+    },
+    'react-native-simple-toast': {},
+    'react-native-code-push': {
+      platforms: {
+        android: {
+          packageInstance:
+            'new CodePush(BuildConfig.CODEPUSH_KEY,getApplicationContext(),BuildConfig.DEBUG)',
+        },
+      },
+    },
+  },
+};
