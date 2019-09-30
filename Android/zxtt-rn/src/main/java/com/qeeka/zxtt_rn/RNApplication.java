00e534f6627686755ba6b9b10ab3d717cf4adc0a
lanstar <lanstar@soundbus.cn>
2019-09-30 17:39:40 +0800

    1、新增Android／zxtt-rn

---
+++ Android/zxtt-rn/src/main/java/com/qeeka/zxtt_rn/RNApplication.java
@@ -0,0 +1,16 @@
+package com.qeeka.zxtt_rn;
+
+import android.app.Application;
+
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+
+import java.util.List;
+
+/**
+ * Created by Even on 2019-09-29
+ */
+public class RNApplication {
+
+
+}
--- examples/android/app/src/main/java/com/zxttrntest/MainActivity.java
+++ examples/android/app/src/main/java/com/zxttrntest/MainActivity.java
@@ -1,15 +1,26 @@
 package com.zxttrntest;
 
-import com.facebook.react.ReactActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
 
-public class MainActivity extends ReactActivity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.qeeka.zxtt_rn.RNActivity;
+
+public class MainActivity extends AppCompatActivity {
 
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
+
   @Override
-  protected String getMainComponentName() {
-    return "SuryaniRN";
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    startActivity(new Intent(this, RNActivity.class));
   }
 }
