c57a7edb980988e66492ff58d44ac884fcf73e3c
林通 <lintong320@gmail.com>
2019-10-12 18:09:34 +0800

    no message

--- examples/android/app/src/main/java/com/zxttrntest/MainActivity.java
+++ examples/android/app/src/main/java/com/zxttrntest/MainActivity.java
@@ -27,8 +27,8 @@ public class MainActivity extends RNActivity {
 //  @Override
 //  protected void onCreate(@Nullable Bundle savedInstanceState) {
 //    super.onCreate(savedInstanceState);
-////    setContentView(R.layout.activity_main);
+//    setContentView(R.layout.activity_main);
 //
-////    startActivity(new Intent(this, RNActivity.class));
+//    startActivity(new Intent(this, RNActivity.class));
 //  }
 }
