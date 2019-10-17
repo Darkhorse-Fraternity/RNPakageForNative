70c78499ecea250257f015838a0c5e29fbbb9d67
林通 <lintong320@gmail.com>
2019-10-17 13:06:00 +0800

    no message

--- src/components/Nav/bar/TSNavBar/index.tsx
+++ src/components/Nav/bar/TSNavBar/index.tsx
@@ -1,26 +1,39 @@
 import React from 'react';
 import {StyledNavbar, StyledArrow, StyledTitle} from './style';
-import {StatusBar, View, TouchableOpacity} from 'react-native';
-
+import {
+  StatusBar,
+  View,
+  TouchableOpacity,
+  GestureResponderEvent,
+} from 'react-native';
+import {useNavigation} from 'react-navigation-hooks';
 // const NavBar = title => {
 //   return <StyledNavbar />;
 // };
 
 interface PropsNavBar {
   title: String;
-  renderLeftView?: Function;
-  renderRightView?: Function;
+  renderLeftView?: (props: PropsNavBar) => Element;
+  renderRightView?: (props: PropsNavBar) => Element;
+  onBackPress?: (event: GestureResponderEvent) => void;
 }
 
-const renderDefaultLeftView = () => {
+const onDefaultBackPress = (event: GestureResponderEvent) => {};
+
+const renderDefaultLeftView = (props: PropsNavBar) => {
+  const {goBack} = useNavigation();
+  const go = () => goBack();
+  const {onBackPress = go} = props;
   return (
-    <TouchableOpacity hitSlop={{top: 30, left: 20, bottom: 20, right: 50}}>
+    <TouchableOpacity
+      onPress={onBackPress}
+      hitSlop={{top: 30, left: 20, bottom: 20, right: 50}}>
       <StyledArrow />
     </TouchableOpacity>
   );
 };
 
-const renderDefaultRightView = () => {
+const renderDefaultRightView = (props: PropsNavBar) => {
   return <View />;
 };
 
@@ -34,9 +47,9 @@ export const NavBar = (props: PropsNavBar) => {
     <>
       <StatusBar backgroundColor="white" barStyle="light-content" translucent />
       <StyledNavbar>
-        {renderLeftView()}
+        {renderLeftView(props)}
         <StyledTitle>{title}</StyledTitle>
-        {renderRightView()}
+        {renderRightView(props)}
       </StyledNavbar>
     </>
   );
