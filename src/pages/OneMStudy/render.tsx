94b8ed2625e3de4693b39c1fde0711f112d24d43
林通 <lintong320@gmail.com>
2019-10-21 11:07:37 +0800

    删除不用代码

--- src/pages/OneMStudy/render.tsx
+++ src/pages/OneMStudy/render.tsx
@@ -2,7 +2,6 @@ import React, {useState, useCallback, useEffect} from 'react';
 import {Background} from './style';
 import {page1} from './questionView/render';
 import {page2} from './resultView/render';
-import {isAbsolute} from 'path';
 import {useFetch} from '../../request/useReq';
 
 import {getQuestionList, getSaveLevel} from '../../request/api';
@@ -11,20 +10,6 @@ import {req} from '../../request';
 const questionListResDefault = {
   number: 0,
   question_list: [],
-};
-
-const fetch = <T extends {}>(params, defaultValue: T): T => {
-  const [data, setData] = useState(defaultValue);
-
-  useEffect(() => {
-    // Update the document title using the browser API
-    const fetchData = async () => {
-      const res = await req(params);
-      setData(res);
-    };
-    fetchData();
-  }, []);
-  return data;
 };
 
 const render = () => {
